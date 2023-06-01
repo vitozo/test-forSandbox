466a97b40befd951423f2e0d11f011cff6b2fb9a
Vitalii Tkachenko <vitozov9@gmail.com>
2023-06-01 17:12:43 -0600

    Try another way

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
 let bmi = (weight, height) => {
 
-  bmi = weight / (height ** 2);
+    bmi = weight / height ** 2;
 
-  if (bmi <= 18.5) {
-    return "Underweight";
-  } else if (bmi <= 25) {
-    return "Normal";
-  } else if (bmi <= 30) {
-    return "Overweight";
-  } else if (bmi > 30) {
-    return "Obese";
-  };
-
+    if (bmi <= 18.5) {
+        return "Underweight";
+    } else if (bmi <= 25.0) {
+        return "Normal";
+    } else if (bmi <= 30.0) {
+        return "Overweight";
+    } else if (bmi > 30) {
+        return "Obese";
+    };
 };
 
+console.log(bmi(80, 1.80));
 
-console.log(bmi(95, 1.83));
+//Another way
+
+let bmiAnother = (weight, height) => {
+
+    bmiAnother = weight / height * height;
+
+    return (
+        bmiAnother <= 18.5 ? "Underweight" : bmiAnother <= 25.0 ? "Normal" : bmiAnother <= 30.0 ? "Overweight" : "Obese"
+    )
+};
+
+console.log(bmiAnother(90, 1.80));
