366e1b4404f1915f542e29e4fe87919c83e001fb
Vitalii Tkachenko <vitozov9@gmail.com>
2023-06-01 11:39:49 -0600

    modified my function

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 let bmi = (weight, height) => {
-    bmi = weight / (height * height);
+
+  bmi = weight / (height ** 2);
+
+  if (bmi <= 18.5) {
+    return "Underweight"
+  }
+
+  else if (bmi <= 25) {
+    return "Normal"
+  }
+
+  else if (bmi <= 30) {
+    return "Overweight"
+  }
+
+  else if (bmi > 30) {
+    return "Obese"
+  }
+
 };
 
-
-bmi (79, 1.83);
+bmi(35, 1.83)
 
 console.log(bmi);
