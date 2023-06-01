a3d0c1c7dc9ff5df362836facf22dcbe46be1419
Vitalii Tkachenko <vitozov9@gmail.com>
2023-06-01 11:44:18 -0600

    formatting code

--- main.js
+++ main.js
@@ -3,23 +3,16 @@ let bmi = (weight, height) => {
   bmi = weight / (height ** 2);
 
   if (bmi <= 18.5) {
-    return "Underweight"
-  }
-
-  else if (bmi <= 25) {
-    return "Normal"
-  }
-
-  else if (bmi <= 30) {
-    return "Overweight"
-  }
-
-  else if (bmi > 30) {
-    return "Obese"
-  }
+    return "Underweight";
+  } else if (bmi <= 25) {
+    return "Normal";
+  } else if (bmi <= 30) {
+    return "Overweight";
+  } else if (bmi > 30) {
+    return "Obese";
+  };
 
 };
 
-bmi(35, 1.83)
 
-console.log(bmi);
+console.log(bmi(95, 1.83));
