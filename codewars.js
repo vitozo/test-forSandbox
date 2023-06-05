c6fd4e7746270e88c7322427356b04250ecc99ff
Vitalii Tkachenko <vitozov9@gmail.com>
2023-06-05 14:08:43 -0600

    New challenge- new way

--- codewars.js
+++ codewars.js
@@ -16,6 +16,17 @@ console.log(findFirstNonConsecutive(arr));
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
-const greet = (name) => name === "Johnny" ? "Hello, my love!" : "Hello," + name + "!";
+// const greet = (name) => name === "Johnny" ? "Hello, my love!" : "Hello," + name + "!";
+
+const greet = (name) => {
+    if (name === "Johnny") {
+        return (
+            "Hello, my love!"
+        );
+    }
+    return(
+        "Hello," + ' ' + name + "!"
+    );
+};
 
 console.log(greet("Din"));
