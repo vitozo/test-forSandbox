ece1a0e5aef1eb3d05cddf28872206161ce22393
Vitalii Tkachenko <vitozov9@gmail.com>
2023-06-07 10:43:25 -0600

    New way to solve a problem

--- codewars.js
+++ codewars.js
@@ -1,12 +1,30 @@
+// const arr = [1, 2, 3, 4, 6, 7, 8];
+
+// const findFirstNonConsecutive = (arr) => {
+
+//     for (let i = 0; i < arr.length - 1; i++) {
+//         const el = arr[i];
+//         const next = arr[i + 1];
+//         if (next - el !== 1) {
+//             return next;
+//         };
+//     };
+//     return null;
+// };
+
 const arr = [1, 2, 3, 4, 6, 7, 8];
 
 const findFirstNonConsecutive = (arr) => {
+    if (arr.length < 2) {
+        return null;
+    };
 
-    for (let i = 0; i < arr.length - 1; i++) {
+    for (let i = 1; i < arr.length; i++) {
         const el = arr[i];
-        const next = arr[i + 1];
-        if (next - el !== 1) {
-            return next;
+        const previous = arr[i - 1];
+        // const next = arr[i + 1];
+        if (el - previous !== 1) {
+            return el;
         };
     };
     return null;
@@ -14,21 +32,3 @@ const findFirstNonConsecutive = (arr) => {
 
 console.log(findFirstNonConsecutive(arr));
 
-////////////////////////////////////////////////////////////////////////////////////////////////
-
-// const greet = (name) => name === "Johnny" ? "Hello, my love!" : "Hello," + name + "!";
-
-// const greet = (name) => {
-//     if (name === "Johnny") {
-//         return (
-//             "Hello, my love!"
-//         );
-//     }
-//     return(
-//         "Hello," + ' ' + name + "!"
-//     );
-// };
-
-const greet = (name) => name === "Johnny" ? "Hello, my love!" : `Hello,  ${name}!`;
-
-console.log(greet("Din"));
--- test.js
+++ test.js
@@ -24,3 +24,44 @@ for (let i = 0; i < cards.length; i++) {
 console.log();
 
 //
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// const greet = (name) => name === "Johnny" ? "Hello, my love!" : "Hello," + name + "!";
+
+// const greet = (name) => {
+//     if (name === "Johnny") {
+//         return (
+//             "Hello, my love!"
+//         );
+//     }
+//     return(
+//         "Hello," + ' ' + name + "!"
+//     );
+// };
+
+const greet = (name) => name === "Johnny" ? "Hello, my love!" : `Hello,  ${name}!`;
+
+console.log(greet("Din"));
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function findNthPower(array, N) {
+    if (N >= 0 && N < array.length) {
+        return Math.pow(array[N], N);
+    } else {
+        return -1;
+    };
+};
+
+
+console.log(findNthPower([1, 2, 3, 4], 2)); // Output: 9
+console.log(findNthPower([1, 2, 3], 3));   // Output: -1
+
+
+
+
+
+
+
